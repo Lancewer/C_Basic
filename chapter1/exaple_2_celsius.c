a8268dba092dd887193485e38fd98aebcbc3760b
Lancewer <lancewer@gmail.com>
2014-10-29 17:28:37 +0800

    add example 2 fahrenheit to celsius

---
+++ chapter1/exaple_2_celsius.c
@@ -0,0 +1,24 @@
+/**
+description: convert fahrenheit to celsius
+*/
+
+#include <stdio.h>
+
+//print out celsius, fahrenheit when fahr=0, 20, ..., 300
+int main()
+{
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while(fahr <= upper){
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr += step;
+    }
+}
+
